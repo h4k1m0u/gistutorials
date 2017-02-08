6729834a03fd3973b6044c818425e50f0c7b3f2c
Hakim Benoudjit <h.benoudjit@gmail.com>
2017-02-08 01:40:07 +0000

    Add author model and its relation with quote

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Quote, Book
+from .models import Quote, Book, Author
+
+
+# customize admin views
+class QuoteAdmin(admin.ModelAdmin):
+    # fields to show in admin listview
+    list_display = ('text', 'author')
+
+
+class BookAdmin(admin.ModelAdmin):
+    # fields to show in admin listview
+    list_display = ('title', 'author')
 
 
 # register models in the admin site
-admin.site.register(Quote)
-admin.site.register(Book)
+admin.site.register(Quote, QuoteAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
--- quotes/models.py
+++ quotes/models.py
@@ -1,9 +1,34 @@
 from django.db import models
 
 
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        abstract = True
+
+
+class Author(Person):
+    def __str__(self):
+        """
+            Field to show in the admin site.
+        """
+        return self.name
+
+
+class Member(Person):
+    username = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     isbn = models.CharField(max_length=10)
+
+    # foreign keys
+    author = models.ForeignKey(Author, null=True, blank=True,
+                               on_delete=models.CASCADE)
 
     def __str__(self):
         """
@@ -13,9 +38,15 @@ class Book(models.Model):
 
 
 class Quote(models.Model):
-    text = models.CharField(max_length=200)
+    text = models.CharField(max_length=1000)
     date = models.DateTimeField(auto_now_add=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    # foreign keys
+    # null for database & blank for forms
+    book = models.ForeignKey(Book, null=True, blank=True,
+                             on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, null=True, blank=True,
+                               on_delete=models.CASCADE)
 
     def __str__(self):
         """
