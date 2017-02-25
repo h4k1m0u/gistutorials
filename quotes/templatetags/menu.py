a32c2c03f52620fad0d94b0017cd1218ec111ec1
Hakim Benoudjit <h.benoudjit@gmail.com>
2017-02-25 21:36:43 +0000

    Show top 5 categories/authors/books by # of quotes in menu

--- quotes/templatetags/menu.py
+++ quotes/templatetags/menu.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 from quotes.models import Category, Book, Author
 
 
@@ -7,9 +8,21 @@ register = template.Library()
 
 @register.inclusion_tag('quotes/menu.html', takes_context=True)
 def menu(context):
+    # url path
+    path = context['request'].path
+
+    # populate menu with top five entries by # of quotes
+    authors = Author.objects.annotate(num_quotes=Count('quote'))\
+        .order_by('-num_quotes')[:5].all()
+    categories = Category.objects.annotate(num_quotes=Count('quote'))\
+        .order_by('-num_quotes')[:5].all()
+    books = Book.objects.annotate(num_quotes=Count('quote'))\
+        .order_by('-num_quotes')[:5].all()
+
     return {
-        'categories': Category.objects.all(),
-        'books': Book.objects.all(),
-        'authors': Author.objects.all(),
+        'categories': categories,
+        'books': books,
+        'authors': authors,
         'search': context.request.GET.get('search', ''),
+        'path': path,
     }
