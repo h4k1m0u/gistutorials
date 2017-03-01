efacc6a0190483673d693d6b9eba07f66256ca36
Hakim Benoudjit <h.benoudjit@gmail.com>
2017-03-01 21:27:31 +0000

    Add credit to footer

--- bookquotes/wsgi.py
+++ bookquotes/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for myquotes project.
+WSGI config for bookquotes project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myquotes.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookquotes.settings")
 
 application = get_wsgi_application()
