80cf6d1f76b1c5ea0e750b1ea4435daa40ecd8b7
Hakim Benoudjit <h.benoudjit@gmail.com>
2017-08-06 23:27:15 +0000

    add success/failure url to social_auth

--- bookquotes/settings.py
+++ bookquotes/settings.py
@@ -316,3 +316,23 @@ SOCIAL_AUTH_FACEBOOK_KEY = '1434286176666478'
 SOCIAL_AUTH_FACEBOOK_SECRET = '25e1daa285a81e3b5596f6320d383456'
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '55725089997-djgj308bu25cpiarmrfn16l2r4iqib0r.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'bKxLfLFedQNneZo31CkyCpOX'
+
+# pipeline of operations
+# http://python-social-auth.readthedocs.io/en/latest/configuration/django.html#personalized-configuration
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'social_core.pipeline.social_auth.associate_by_email',
+)
+
+# urls to redirect to on success/failure
+
+SOCIAL_AUTH_LOGIN_URL = '/'
+SOCIAL_AUTH_LOGIN_ERROR_URL = '/'
