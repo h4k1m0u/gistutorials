ca4a3b528ec176f770f7cd87d401723cb300af1f
Hakim Benoudjit <h.benoudjit@gmail.com>
2017-08-11 20:56:04 +0000

    Add dropdown menu to quotes menu item

--- templatetags/menu.py
+++ templatetags/menu.py
@@ -1,6 +1,4 @@
 from django import template
-from django.db.models import Count
-from quotes.models import Category, Book, Author
 
 
 register = template.Library()
@@ -11,21 +9,10 @@ def menu(context):
     # app name
     app_name = context['request'].resolver_match.app_name
 
-    # populate menu with top five entries by # of quotes
-    authors = Author.objects.annotate(num_quotes=Count('quote'))\
-        .order_by('-num_quotes')[:5].all()
-    categories = Category.objects.annotate(num_quotes=Count('quote'))\
-        .order_by('-num_quotes')[:5].all()
-    books = Book.objects.annotate(num_quotes=Count('quote'))\
-        .order_by('-num_quotes')[:5].all()
-
     # current user
     user = context['user']
 
     return {
-        'categories': categories,
-        'books': books,
-        'authors': authors,
         'search': context.request.GET.get('search', ''),
         'user': user,
         'app_name': app_name,
