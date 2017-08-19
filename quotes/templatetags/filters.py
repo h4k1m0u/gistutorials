1cfc5d7f6ad67f72bd6507e3362861daff4b3e83
Hakim Benoudjit <h.benoudjit@gmail.com>
2017-08-19 09:30:38 +0000

    Dont show browsed category/book/author on sidebar

--- quotes/templatetags/filters.py
+++ quotes/templatetags/filters.py
@@ -8,16 +8,6 @@ register = template.Library()
 
 @register.inclusion_tag('quotes/filters.html', takes_context=True)
 def filters(context):
-    # browsed author, book, category (when not in auth forms)
-    view = context.get('view')
-    if view:
-        kwargs = view.kwargs
-        browsed_author = kwargs.get('author')
-        browsed_book = kwargs.get('book')
-        browsed_category = kwargs.get('category')
-    else:
-        browsed_author = browsed_book = browsed_category = None
-
     # populate menu with top five entries by # of quotes
     authors = Author.objects.annotate(num_quotes=Count('quote'))\
         .order_by('-num_quotes')[:5].all()
@@ -27,9 +17,6 @@ def filters(context):
         .order_by('-num_quotes')[:5].all()
 
     return {
-        'browsed_author': browsed_author,
-        'browsed_book': browsed_book,
-        'browsed_category': browsed_category,
         'categories': categories,
         'books': books,
         'authors': authors,
