0a613bef87ef4b678ae042d00d37e03cb02482bb
Hakim Benoudjit <h.benoudjit@gmail.com>
2017-12-31 18:19:44 +0000

    REST API for articles in given month

--- articles/urls.py
+++ articles/urls.py
@@ -1,18 +1,4 @@
-from django.conf.urls import url
-from . import views
-
-
 app_name = 'articles'
 
 urlpatterns = [
-    # articles list view
-    url(r'^$', views.ArticlesListView.as_view(), name='articles-list'),
-    url(r'^tag/(?P<tag>[-\w]+)/$', views.ArticlesListView.as_view(),
-        name='articles-list'),
-    url(r'^member/(?P<member>[-\w]+)/$', views.ArticlesListView.as_view(),
-        name='articles-list'),
-
-    # article detail view
-    url(r'^article/(?P<pk>[0-9]+)/(?P<slug>[-\w]*)$',
-        views.ArticlesDetailView.as_view(), name='article-detail'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,8 @@ from django.views.generic import ListView, DetailView
 from .models import Article
 from rest_framework import viewsets
 from .serializers import ArticleSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import list_route
 
 
 class ArticlesListView(ListView):
@@ -47,3 +49,15 @@ class ArticleViewSet(viewsets.ModelViewSet):
     """
     queryset = Article.published_objects.all().order_by('-date')
     serializer_class = ArticleSerializer
+
+    @list_route(url_path='month/(?P<month>[0-9]|10|11|12)')
+    def month(self, request, month):
+        """
+        Articles published in the given month.
+        This method is accessed with: /articles/month/xx/
+        """
+        # filter by month
+        month = self.kwargs.get('month')
+        qs = Article.objects.filter(date__month=month)
+
+        return Response(ArticleSerializer(qs, many=True).data)
