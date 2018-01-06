bc488621ada82418bd140c62d6b4f19666030a79
Hakim Benoudjit <h.benoudjit@gmail.com>
2018-01-06 18:32:48 +0000

    Get years, months containing articles

--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from .serializers import ArticleSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import list_route
+from django.db.models.functions import ExtractMonth, ExtractYear
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
@@ -13,14 +14,28 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.published_objects.all().order_by('-date')
     serializer_class = ArticleSerializer
 
-    @list_route(url_path='month/(?P<month>[0-9]|10|11|12)')
-    def month(self, request, month):
+    @list_route(url_path='date/(?P<year>[0-9]{4})/(?P<month>[0-9]|10|11|12)')
+    def by_date(self, request, year, month):
         """
-        Articles published in the given month.
-        This method is accessed with: /articles/month/xx/
+        Articles published in the given month of the given year.
+        This method is accessed with: /articles/date/yyyy/mm/
         """
         # filter by month
         month = self.kwargs.get('month')
-        qs = Article.objects.filter(date__month=month)
+        year = self.kwargs.get('year')
+        qs = Article.objects.filter(date__month=month, date__year=year)
 
         return Response(ArticleSerializer(qs, many=True).data)
+
+    @list_route()
+    def dates(self, request):
+        """
+        Get (years, months) containing articles.
+        """
+        # filter by month
+        qs = Article.objects.annotate(
+            month=ExtractMonth('date'),
+            year=ExtractYear('date')
+        ).values_list('year', 'month')
+
+        return Response(set(qs))
