aacc9e4843f876695096dd13578495cf483a6edc
Hakim Benoudjit <h.benoudjit@gmail.com>
2018-01-29 20:01:14 +0000

    Get tags with article in Rest API

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,8 +1,16 @@
-from .models import Article
+from .models import Article, Tag
 from rest_framework import serializers
 
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+
+
 class ArticleSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
+
     class Meta:
         model = Article
-        fields = ('id', 'title', 'text', 'date', 'image')
+        fields = ('id', 'title', 'text', 'date', 'image', 'tags')
