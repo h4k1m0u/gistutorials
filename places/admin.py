32b814cf2f4744d45f6a3de360ca4f3f04d546a7
Hakim Benoudjit <h.benoudjit@gmail.com>
2018-02-25 20:15:22 +0000

    Media for Create a GIS database with PostgreSQL and PostGIS tutorial

--- places/admin.py
+++ places/admin.py
@@ -1,17 +1,9 @@
 from django.contrib.gis import admin
 from .models import City
+from leaflet.admin import LeafletGeoAdmin
 
 
-# OSMGeoAdmin allows to get annotated OSM maps in admin widgets
-class CityAdmin(admin.OSMGeoAdmin):
-    """
-    OSMGeoAdmin allows to get annotated OSM maps in admin widgets.
-    http://blog.adamfast.com/2011/11/improving-the-admin/
-    """
-    # Algeria's (lon:3, lat:36) in epsg:3857 (web maps) projection
-    default_lon = 333958.47237982077
-    default_lat = 4300621.372044271
-
+class CityAdmin(LeafletGeoAdmin):
     # fields to show in admin listview
     list_display = ('name', 'geometry')
 
